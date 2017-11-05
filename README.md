e8fcb0fd731e49b0af508ca6ed7596ff06d3cb8d
fergonara <fergonzalez2910@gmail.com>
2017-11-05 16:33:50 -0600

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Online Address: [Vue Resume](http://resume.ido321.com/).
+
+ https://github.com/dwqs/vue-resume.git 
---
+++ client/components/FloatRight.vue
@@ -0,0 +1,86 @@
+<template>
+    <aside class="float-right">
+        <ul>
+            <li v-for="item in items">
+                <a :href="item.link" @click.stop="printResume(item.isPrint)" target="_blank">{{item.text}}</a>
+            </li>
+        </ul>
+    </aside>
+</template>
+
+<style scoped lang="less">
+    @media print {
+        .float-right {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width: 1140px){
+        .float-right{
+            position: fixed;
+            right: 0;
+            top: 30%;
+            z-index: 2;
+            li{
+                margin-bottom: 10px;
+                border-radius: 10px 0 0 10px;
+                box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
+                background-color: #00BFA5;
+                cursor: pointer;
+                a{
+                    display: inline-block;
+                    width: 80px;
+                    height: 38px;
+                    line-height: 38px;
+                    padding-left: 10px;
+                    color: rgba(255,255,255,0.8);
+                    transition: all .3s ease-out;
+                    &:hover{
+                        color: rgba(255,255,255,1);
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (max-width: 1140px){
+        .float-right{
+            display: none;
+        }
+    }
+</style>
+
+<script>
+
+    export default{
+        data (){
+            return{
+                items:[{
+                    text:"Código",
+                    link:"https://github.com/fergo2910/vue-resume-fergonara.git",
+                    isPrint: false
+                },{
+                    text:"Github",
+                    link:"https://github.com/fergo2910",
+                    isPrint:false
+                },{
+                    text:"PDF",
+                    link:"/袁康-前端开发.pdf",
+                    isPrint:false
+                },{
+                    text:"Imprimir",
+                    link:"#",
+                    isPrint:true
+                }]
+            };
+        },
+
+        methods:{
+            printResume(isPrint){
+                if(isPrint){
+                    window.print();
+                }
+            }
+        }
+    };
+</script>
